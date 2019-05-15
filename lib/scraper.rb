d57fe17e0caee1a897a026d72d21b67a9ca298d9
Solomon Feldman <solomonrfeldman@gmail.com>
2019-05-15 19:36:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,16 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
   
-    def print_courses
+  def get_page
+    doc = Nokogiri::HTML(open(""))
+  end
+  
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
